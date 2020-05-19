b1c95a1d7d46495d36cfbb5f330b303ab8cc22cd
MichaelZhuk <64740409+MichaelZhuk@users.noreply.github.com>
2020-05-19 07:54:53 +0300

    new3

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const choises = document.getElementsByClassName("choise");
+const animals = document.getElementsByClassName("animal");
+const money = document.getElementById("money");
+const walking = document.getElementById("walking");
+const stroke = document.getElementById("stroke");
+const timer = document.getElementById("timer");
 
+for (let i = 0; i < choises.length; i++) {
+    choises[i].onclick = onChoisesClick;
+    function onChoisesClick() {
+       
+    }
+}
+
+for (let i = 0; i < animals.length; i++) {
+    animals[i].onclick = onAnimalClick;
+    function onAnimalClick() {
+       
+    }
+}
